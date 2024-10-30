b16e7011726e2464679a23a7d24a8d98ccee844a
syh213 <syh21133@gmail.com>
2024-10-30 14:49:51 +0900

    refactor : 기능 개선
    레벨 2
    SignupRequest
    유효성 검사 추가
    이메일 , 비밀번호

--- src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/org/example/expert/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignupRequest {
 
-    @NotBlank @Email
+    @NotBlank(message = "사용자 아이디는 필수입니다.")
+    @Email(message = "이메일 형식이어야 합니다.")
     private String email;
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+])[A-Za-z\\d!@#$%^&*()_+]{8,}$", message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함한 8글자 이상으로 설정하세요")
     private String password;
     @NotBlank
     private String userRole;
